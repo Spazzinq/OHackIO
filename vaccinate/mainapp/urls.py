2ebed70ca5b0941dbc470485b8344e82e6841987
zinodyta1 <44512495+zinodyta1@users.noreply.github.com>
2021-03-20 17:14:43 -0400

    added database and main page

--- vaccinate/mainapp/urls.py
+++ vaccinate/mainapp/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.providerForm,name="providerForm"),
+    path('',views.mainpage,name="mainpage"),
+    path('providerForm/',views.providerForm,name="providerForm"),
     path('formresponse/',views.formresult,name="formresult"),
     path('providerDashboard/',views.providerDashboard,name="providerDashboard"),
     path('submitted/',views.submitted,name="submitted")
