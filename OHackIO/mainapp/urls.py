b696e016d467ba82d66290fda1c053f925932d4e
zinodyta1 <44512495+zinodyta1@users.noreply.github.com>
2021-03-20 13:07:28 -0400

    started setting up forms

--- OHackIO/mainapp/urls.py
+++ OHackIO/mainapp/urls.py
@@ -1,6 +1,6 @@
-from django.urls import paths
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name="index")
+    path('',views.providerForm,name="providerForm")
 ]
--- OHackIO/mainapp/views.py
+++ OHackIO/mainapp/views.py
@@ -1,3 +1,23 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render
+from django import forms
 
-# Create your views here.
+
+class ProviderLogin(forms.Form):
+    name = forms.CharField(label="Provider Name")
+    address = forms.CharField(label="Provider Address")
+
+
+def providerForm(request):
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            provider_name = form.cleaned_data['name']
+            provider_address = form.cleaned_data['address']
+            return HttpResoibsedRedurst(reverse("formresult"))
+
+    return render(request,"formtesting.html",{"form":ProviderLogin})
+
+def formresult(request):
+    return render(request,"formresulttest.html", {"test":(provider_name + " " + provider_address)})
