2d350d3280d06a969a2f8f89e427cef8b18c50d8
zinodyta1 <44512495+zinodyta1@users.noreply.github.com>
2021-03-20 15:22:15 -0400

    fix typo + merge stuff

--- vaccinate/mainapp/views.py
+++ vaccinate/mainapp/views.py
@@ -20,4 +20,29 @@ def providerForm(request):
     return render(request,"formtesting.html",{"form":ProviderLogin})
 
 def formresult(request):
-    return render(request,"formresulttest.html", {"test":(provider_name + " " + provider_address)})
+
+    name = request.session['provider_name']
+    address = request.session['provider_address']
+    return render(request,"formresulttest.html", {"test":(name + " " + address)})
+
+def providerDashboard(request):
+    name = request.session['provider_name']
+    address = request.session['provider_address']
+
+    if request.method == "POST":
+        form = AppointmentInfo(request.POST)
+        if form.is_valid():
+            appDate = form.cleaned_data['date']
+            appTime = form.cleaned_data['time']
+            appType = form.cleaned_data['type']
+
+            DataTime = str(appDate) + " " + str(appTime)
+            #put this data into database
+            return HttpResponseRedirect(reverse("submitted"))
+
+
+    return render(request,"providerDashboard.html",{"providername" :name, "form":AppointmentInfo})
+
+
+def submitted(request):
+    return render(request,"submitted.html")
