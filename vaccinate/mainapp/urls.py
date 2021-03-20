af0c202094598bb4a87be0e299bdff95fe92dbed
zinodyta1 <44512495+zinodyta1@users.noreply.github.com>
2021-03-20 18:16:05 -0400

    change url of providerForm

--- vaccinate/mainapp/urls.py
+++ vaccinate/mainapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.providerForm, name="providerForm"),
+    path('providerForm/',views.providerForm, name="providerForm"),
     path('map/',views.getData, name="getData")
 ]
